a5c3808f90aaf3008bf0ca4b7473e115c616ff86
AdamAinsworthCHS <147109448+AdamAinsworthCHS@users.noreply.github.com>
2025-12-09 08:50:59 -0800

    added some comments to dialogue script

--- Twisted Petal/Assets/Scripts/Dialogue.cs
+++ Twisted Petal/Assets/Scripts/Dialogue.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class Dialogue : MonoBehaviour
 {
-    private int dialogueLength;
-    private string displayedDialogue;
-    public List<string> dialogueLines;
-    private bool talking = false;
-    private int currentLine = 0;
-    private float dialogueDelay = 0f;
-    private int currentCharacter = 0;
-    public static float textSpeed = 0.05f;
+    private int dialogueLength; // the amount of characters in the current dialogue line
+    private string displayedDialogue; // the text currently displayed in the dialogue box
+    public List<string> dialogueLines; // every line of dialogue for this dialogue box
+    private bool talking = false; // whether or not dialogue is being printed
+    private int currentLine = 0; // the current line of dialogue being printed
+    private float dialogueDelay = 0f; // used to keep track of when new characters can be printed
+    private int currentCharacter = 0; // the id of the current character being printed
+    public static float textSpeed = 0.05f; // The delay in between characters getting printed (in seconds)
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
