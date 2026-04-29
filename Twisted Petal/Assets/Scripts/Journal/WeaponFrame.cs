628965c51901905d40e534bfb022ef4118e38fab
AdamAinsworthCHS <147109448+AdamAinsworthCHS@users.noreply.github.com>
2026-04-29 15:58:01 -0700

    journal now uses ? sprite instead of white box for undiscovered weapons

--- Twisted Petal/Assets/Scripts/Journal/WeaponFrame.cs
+++ Twisted Petal/Assets/Scripts/Journal/WeaponFrame.cs
@@ -9,6 +9,7 @@ public class WeaponFrame : MonoBehaviour
     private DataManagement saveData;
     private bool loopDone;
     private WeaponsFound weaponsFound;
+    public GameObject placeholderImage;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -28,6 +29,10 @@ public class WeaponFrame : MonoBehaviour
             if (weaponsFound.foundWeapons.Contains(itemStored))
             {
                 this.GetComponent<Image>().sprite = frameImage;
+            }
+            else
+            {
+                this.GetComponent<Image>().sprite = placeholderImage.GetComponent<SpriteRenderer>().sprite;
             }
             loopDone = true;
         }
